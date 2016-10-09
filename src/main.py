6e03a84001a853ecb78e7d1592da2d6a60903bef
Sage.Smith <sage.smith@workiva.com>
2016-10-08 21:06:05 -0600

    Created homepage

--- src/main.py
+++ src/main.py
@@ -1,42 +1,54 @@
-import logging
+import datetime
 
 import bottle
-from bottle import route
+from bottle import route, post
 from core import respond
-import json
-from generate_test_game import generate_game
+from generate_test_game import generate_game, generate_games
 
 bottle.debug(True)
 app = bottle.default_app()
 
 
 @route('/')
-def home():
-    msg = "Sage and Bridger will add stuff here."
-    logging.info('someone hit the home page')  # app engine log.
-    return respond('index.html', {'msg': msg})
+@route('/<games>')
+@route('/<games>/')
+@post('/')
+@post('/<games>')
+@post('/<games>/')
+def home(games=None):
+    today = bottle.request.forms.get('date')
+    if today is not None:
+        year, month, day = today.split('-')
+    else:
+        today = datetime.datetime.now()
+        day = today.day
+        month = today.month
+        year = today.year
+
+    return respond(
+        'index.html',
+        {
+            'games': generate_games(),
+            'day': day,
+            'month': month,
+            'year': year
+        }
+    )
 
 
-@route('/test_game_data')
-@route('/test_game_data/')
-def test_game():
-    return respond('index.html', {'msg': json.dumps(generate_game(), indent=4)})
-
-
-@route('/view_game/<month>/<day>/<year>/<home>/<game_number>')
-@route('/view_game/<month>/<day>/<year>/<home>/<game_number>/')
-@route('/view_game/<month>/<day>/<year>/<home>/')
-@route('/view_game/<month>/<day>/<year>/<home>')
-def view_game(month, day, year, home, game_number=None):
+@route('/view_game/<game_id>')
+@route('/view_game/<game_id>/')
+def view_game(game_id):
     game = generate_game()
+
     return respond(
         'view-game.html',
         {
-            'home': home.upper(),
+            'home': game['home'].upper(),
             'away': game['away'].upper(),
-            'day': day,
-            'month': month,
-            'year': year,
+            'day': game['day'],
+            'month': game['month'],
+            'year': game['year'],
             'game_data': generate_game()
         }
     )
